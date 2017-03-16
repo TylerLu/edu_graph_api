402fcba63cb5f03f4b1358c9bcea33fd9f299aec
姚露 <lu.yao@mljr.com>
2017-03-16 18:45:02 +0800

    optimize

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,19 +9,21 @@ module ApplicationHelper
   	if File.exist? "#{Rails.root}/public/photos/#{objectId}.jpg"
 			photo_url = "/photos/#{objectId}.jpg"
 		else
-			photo = HTTParty.get("https://graph.microsoft.com/v1.0/users/#{objectId}/photo/$value", headers: {
-				"Authorization" => "#{session[:token_type]} #{session[:gmc_access_token]}",
-				"Content-Type" => "application/x-www-form-urlencoded"
-			}).body
+			# photo = HTTParty.get("https://graph.microsoft.com/v1.0/users/#{objectId}/photo/$value", headers: {
+			# 	"Authorization" => "#{session[:token_type]} #{session[:gmc_access_token]}",
+			# 	"Content-Type" => "application/x-www-form-urlencoded"
+			# }).body
 
-			if photo.nil? || photo.empty?
-				FileUtils.cp("#{Rails.root}/public/Images/header-default.jpg", "#{Rails.root}/public/photos/#{objectId}.jpg")
-			else
-				File.open("#{Rails.root}/public/photos/#{objectId}.jpg", "wb") do |f|
-					f.write photo
-				end
-			end
-			photo_url = "/photos/#{objectId}.jpg"	
+			# if photo.nil? || photo.empty?
+			# 	FileUtils.cp("#{Rails.root}/public/Images/header-default.jpg", "#{Rails.root}/public/photos/#{objectId}.jpg")
+			# else
+			# 	File.open("#{Rails.root}/public/photos/#{objectId}.jpg", "wb") do |f|
+			# 		f.write photo
+			# 	end
+			# end
+			# photo_url = "/photos/#{objectId}.jpg"	
+			SyncUserPhotoJob.perform_later(objectId, session[:token_type], session[:gmc_access_token], "#{Rails.root}/public/photos")
+			photo_url = "/Images/header-default.jpg"
 		end
 
 		return photo_url
---
+++ app/jobs/sync_user_photo_job.rb
@@ -0,0 +1,19 @@
+class SyncUserPhotoJob < ApplicationJob
+  queue_as :default
+
+  # 开启后端任务， 异步抓住用户头像
+  def perform(objectId, token_type, access_token, fiel_path)
+    photo = HTTParty.get("https://graph.microsoft.com/v1.0/users/#{objectId}/photo/$value", headers: {
+			"Authorization" => "#{token_type} #{access_token}",
+			"Content-Type" => "application/x-www-form-urlencoded"
+		}).body
+
+
+		unless photo.nil? || photo.empty?
+			# File.open("#{Rails.root}/public/photos/#{objectId}.jpg", "wb") do |f|
+			File.open("#{fiel_path}/#{objectId}.jpg", "wb") do |f|
+				f.write photo
+			end
+		end
+  end
+end
